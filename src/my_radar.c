49dce25919312df7355dee96122da7cd88e833f3
Tom C. <tom.carlier@epitech.eu>
2024-01-09 11:53:40 +0100

    [+] Ad the env checking

--- src/my_radar.c
+++ src/my_radar.c
@@ -7,9 +7,21 @@
 
 #include "my.h"
 
+static int check_env(char **envp)
+{
+    for (int i = 0; env[i]; i++)
+        if (my_strncmp(env[i], "DISPLAY", 6))
+            return 0;
+    return 84;
+}
+
 int main(int argc, char **argv, char **envp)
 {
-    if (argc == 2 && !my_strcmp(argv[1], "-h"))
+    if (check_env(envp))
+        return 84;
+    if (argc == 2 && my_strlen(argv[2]) == 2 && !my_strncmp(argv[1], "-h", 2))
         return display_usage();
+    if (argc == 2)
+        return open_file(argv[1]);
     return 84;
 }
